97b0e0b6aab36597e5f5f2a27a84d717d61a3b9f
villafrade <cavillafrades@gmail.com>
2022-06-08 11:05:00 -0500

    [PWA]deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA ({
+    pwa:{
+        dest:'public',
+        register:true,
+        mode:'production',
+        disable:false
+    },
+    
     reactStringMode:true,
     images:{
         domains:['api.lorem.space'],
     }
-}
+})
